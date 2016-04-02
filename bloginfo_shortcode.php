c5ffb953ca3ebd40b295b234d6c5c4bed914b06b
djyugg <github@n741x1.com>
2016-04-02 22:39:28 +0900

    initial commit

---
+++ bloginfo_shortcode.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: bloginfo shortcode
+Description: Use a template tag "bloginfo" as a shortcode
+Version: 0.0.1
+Author: djyugg
+Author URI: http://about.djyugg.me/
+License: MIT
+*/
+
+function getBloginfoTemplateUrl()
+{
+    return bloginfo('template_url');
+}
+
+add_shortcode('bloginfo_template_url', 'getBloginfoTemplateUrl');
